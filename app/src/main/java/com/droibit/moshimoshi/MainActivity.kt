dc581481a1502535c7820571be5641a046761f67
Shinya Kumagai <roomful.rooms@gmail.com>
2019-10-17 21:41:14 +0900

    MIgrate to androidx

--- app/src/main/java/com/droibit/moshimoshi/MainActivity.kt
+++ app/src/main/java/com/droibit/moshimoshi/MainActivity.kt
@@ -2,7 +2,7 @@ package com.droibit.moshimoshi
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import com.droibit.moshimoshi.adapter.UriAdapter
 import com.droibit.moshimoshi.entity.Article
